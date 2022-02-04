3981502343ffcb476692e05782bd5f36364f1a7b
Iandmee <milaevpro1@gmail.com>
2022-02-04 22:08:48 +0300

    final interfaces

--- include/R6502.h
+++ include/R6502.h
@@ -16,25 +16,33 @@ namespace NES {
             };
             char flags{0};
         };
+//Regs:
         uint8_t A{0}, X{0}, Y{0}, S{0};
         uint16_t PC{0};
         uint8_t P{0};
-        uint8_t RAM[1<<13];
+        uint8_t RAM[1 << 13];
+//Addressing Modes:
 
-        [[maybe_unused]] uint8_t immediate();
+        uint8_t immediate();
 
-        [[maybe_unused]] uint8_t zero_page();
+        uint8_t zero_page();
 
-        [[maybe_unused]] uint8_t zero_page_x();
+        uint8_t zero_page_x();
 
-        [[maybe_unused]] uint8_t zero_page_y();
+        uint8_t zero_page_y();
 
-        [[maybe_unused]] uint8_t relative();
+        uint8_t relative();
+
         uint8_t absolute();
+
         uint8_t absolute_x();
+
         uint8_t absolute_y();
+
         uint8_t indirect();
+
         uint8_t indexed_indirect();
+
         uint8_t inderect_indexed();
 
     };
--- src/R6502.cpp
+++ src/R6502.cpp
@@ -1,24 +1,49 @@
 #include "R6502.h"
 namespace NES{
 
-    [[maybe_unused]] uint8_t CPU::immediate() {
+     uint8_t CPU::immediate() {
         return 0;
     }
 
-    [[maybe_unused]] uint8_t CPU::zero_page() {
+     uint8_t CPU::zero_page() {
         return 0;
     }
 
-    [[maybe_unused]] uint8_t CPU::zero_page_x() {
+    uint8_t CPU::zero_page_x() {
         return 0;
     }
 
-    [[maybe_unused]] uint8_t CPU::zero_page_y() {
+    uint8_t CPU::zero_page_y() {
         return 0;
     }
 
-    [[maybe_unused]] uint8_t CPU::relative() {
+    uint8_t CPU::relative() {
         return 0;
     }
+
+    uint8_t CPU::absolute() {
+        return 0;
+    }
+
+    uint8_t CPU::absolute_x() {
+        return 0;
+    }
+
+    uint8_t CPU::absolute_y() {
+        return 0;
+    }
+
+    uint8_t CPU::indirect() {
+        return 0;
+    }
+
+    uint8_t CPU::indexed_indirect() {
+        return 0;
+    }
+
+    uint8_t CPU::inderect_indexed() {
+        return 0;
+    }
+
 }
 
