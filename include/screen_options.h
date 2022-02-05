1e5fee6f8b3ba3d5577194a384c7ee206be8e2e1
Dan Nesterov <kusosuha6vt@gmail.com>
2022-02-05 07:35:12 +0300

    Added basic screen

---
+++ include/screen_options.h
@@ -0,0 +1,11 @@
+#ifndef NADNES_SCREEN_OPTIONS_H
+#define NADNES_SCREEN_OPTIONS_H
+
+namespace NADNESS {
+
+const int WIDTH = 256;
+const int HEIGHT = 240;
+
+}  // namespace NES
+
+#endif  // NADNES_SCREEN_OPTIONS_H
