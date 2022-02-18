0d3f86368abd156785cb5697827e9ec2b97c8eec
Dan Nesterov <kusosuha6vt@gmail.com>
2022-02-18 21:10:45 +0300

    21:10

--- include/Apu.h
+++ include/Apu.h
@@ -2,15 +2,26 @@
 #define NADNES_APU_H
 
 #include <cstdint>
+#include <cassert>
 
 namespace NADNES {
+
+struct Timer {
+    //
+};
 
 class Apu {
 public:
     Apu();
     ~Apu();
 
-    private:
+    void write(std::uint16_t addr, std::uint8_t value) {
+
+    }
+private:
+    struct Pulse {
+
+    };
 };
 
 }
