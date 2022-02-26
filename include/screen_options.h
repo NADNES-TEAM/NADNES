05287c615b6b7599ea23e8b49e97ac754594a337
Dan Nesterov <kusosuha6vt@gmail.com>
2022-02-26 21:13:45 +0300

    pragma once

--- include/screen_options.h
+++ include/screen_options.h
@@ -1,6 +1,4 @@
-#ifndef NADNES_SCREEN_OPTIONS_H
-#define NADNES_SCREEN_OPTIONS_H
-
+#pragma once
 namespace NES {
 
 const int WIDTH = 256;
@@ -8,5 +6,3 @@ const int HEIGHT = 240;
 const int WINDOW_SCALE = 3;
 
 }  // namespace NES
-
-#endif  // NADNES_SCREEN_OPTIONS_H
