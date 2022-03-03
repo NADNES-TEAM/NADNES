7bfc7c0c3f1340561ea644c785d0f6fb927d50f2
Iandmee <milaevpro1@gmail.com>
2022-03-03 20:30:46 +0300

    fixes

--- include/bus.h
+++ include/bus.h
@@ -2,12 +2,20 @@
 #define BUS_H
 #include <cstdint>
 #include <array>
+#include "cartridge.h"
+#include "connect_token.h"
+#include "PPU.h"
 namespace NES {
-    struct Bus {
-        std::array<uint8_t, 1<<13> RAM{};
-        std::array<uint8_t, 1<<13> SRAM{};
-        uint8_t mem_read(uint16_t addr);
-        void mem_write(uint16_t addr, uint8_t data);
-    };
+struct Bus {
+    std::array<uint8_t, 1<<11> RAM{};
+    uint8_t mem_read(uint16_t addr);
+    void mem_write(uint16_t addr, uint8_t data);
+    //Connected devices
+    Ppu *ppu;
+    CpuToCartridgeInterface  *cartridge;
+
+    void connect(CpuToCartridgeInterface *cartridge_, ConnectToken) noexcept;
+    void connect(Ppu *ppu_, ConnectToken) noexcept;
+};
 }
 #endif //BUS_H
--- src/bus.cpp
+++ src/bus.cpp
@@ -1,10 +1,64 @@
 #include "bus.h"
-namespace NES{
-    void Bus::mem_write(uint16_t addr, uint8_t data) {
 
+namespace NES {
+
+void Bus::mem_write(uint16_t addr, uint8_t data) {
+    if (addr >= 0 && addr < 0x2000) {  // RAM
+        RAM[addr % 0x800] = data;
+    } else if (addr >= 0x2000 && addr < 0x4000) {  // PPU Ctrl regs
+        if (addr == 0x2000) {
+            ppu->write_ctrl_reg(data);
+        } else if (addr == 0x2001) {
+            ppu->write_mask_reg(data);
+        } else if (addr == 0x2003) {
+            ppu->set_OAM_address(data);
+        } else if (addr == 0x2004) {
+            ppu->OAM_write(data);
+        } else if (addr == 0x2005) {
+            ppu->write_scroll_reg(data);
+        } else if (addr == 0x2006) {
+            ppu->set_VRAM_address(data);
+        } else if (addr == 0x2007) {
+            ppu->VRAM_write(data);
+        }
     }
-    uint8_t Bus::mem_read(uint16_t addr) {
+    if (addr >= 0x4000 && addr < 0x4020) {  // Controllers + APU
+    }
+    if ((addr >= 0x4020 && addr < 0x6000) || addr >= 0x8000) {  // ROM
+        cartridge->CPU_write(addr, data);
+    }
+    if (addr >= 0x6000 && addr < 0x8000) {  // SRAM - just do nothing
 
+        return;
     }
 }
+uint8_t Bus::mem_read(uint16_t addr) {
+    if (addr < 0x2000) {  // RAM
+        return RAM[addr % 0x800];
+    }
+    if (addr >= 0x2000 && addr < 0x4000) {  // PPU Ctrl regs
+        if (addr == 0x2002) {
+            return ppu->read_status_reg();
+        } else if (addr == 0x2004) {
+            return ppu->OAM_read();
+        } else if (addr == 0x2007) {
+            return ppu->VRAM_read();
+        }
+    }
+    if (addr >= 0x4000 && addr < 0x4020) { // Controllers + APU
+    }
+    if ((addr >= 0x4020 && addr < 0x6000) || addr >= 0x8000) {  // ROM
 
+        return cartridge->CPU_read(addr);
+    }
+    if (addr >= 0x6000 && addr < 0x8000) {  // SRAM - just do nothing
+        return 0;
+    }
+}
+void Bus::connect(CpuToCartridgeInterface *cartridge_, ConnectToken) noexcept {
+    cartridge = cartridge_;
+}
+void Bus::connect(Ppu *ppu_, ConnectToken) noexcept {
+    ppu = ppu_;
+}
+}
