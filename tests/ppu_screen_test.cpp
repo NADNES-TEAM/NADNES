b4cf3a19bb5a7850b45bc10c8bbc868cb6f4bfa4
das747 <40293014+das747@users.noreply.github.com>
2022-03-09 00:21:00 +0300

    rebased on dev

--- tests/ppu_screen_test.cpp
+++ tests/ppu_screen_test.cpp
@@ -2,17 +2,23 @@
 #include <QPushButton>
 #include <QTimer>
 #include <chrono>
+#include <iostream>
 #include <thread>
 #include "nes.h"
+#include "nes_exceptions.h"
 #include "screen.h"
 
 int main(int argc, char *argv[]) {
-    QApplication application(argc, argv);
+    try {
+        QApplication application(argc, argv);
 
-    NES::Screen screen;
-    NES::Nes nes("C:\\Users\\milae\\Trash_Downloads\\nestest.nes", &screen);
-    auto *timer = new QTimer(&application);
-    QObject::connect(timer, &QTimer::timeout, &nes, &NES::Nes::tick, Qt::DirectConnection);
-    timer->start(std::chrono::milliseconds(1000 / 60));
-    return QApplication::exec();
+        NES::Screen screen;
+        NES::Nes nes("../nestest.nes", &screen);
+        auto *timer = new QTimer(&application);
+        QObject::connect(timer, &QTimer::timeout, &nes, &NES::Nes::tick, Qt::DirectConnection);
+        timer->start(std::chrono::milliseconds(1000 / 60));
+        return QApplication::exec();
+    } catch (const std::exception &e) {
+        std::cout << e.what();
+    }
 }
