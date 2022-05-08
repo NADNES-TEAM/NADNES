eab51df5f932ea38162abbbdc3382394a67e5d00
das747 <40293014+das747@users.noreply.github.com>
2022-05-08 21:32:28 +0300

    changed key input to sequence edit

--- include/environment/keymap_window.h
+++ include/environment/keymap_window.h
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <QKeySequenceEdit>
 #include <QMap>
 #include <QPushButton>
 #include <QTableWidget>
@@ -12,20 +13,22 @@ class KeymapWindow : public QWidget {
     Q_OBJECT
 
 public:
-    KeymapWindow(QMap<Qt::Key, NES::Keys> &key_to_btn);
+    explicit KeymapWindow(QMap<Qt::Key, NES::Keys> &key_to_btn);
     ~KeymapWindow() override = default;
 
 private slots:
-    void on_table_cellChanged(int row, int col);
     void on_restore_btn_clicked();
     void on_cancel_btn_clicked();
     void on_apply_btn_clicked();
     void on_save_btn_clicked();
+    void on_sequence_edit_editingFinished();
+    void on_table_widget_itemDoubleClicked();
 
 private:
-    QTableWidget *table;
+    QTableWidget *m_table;
     QMap<NES::Keys, Qt::Key> btn_to_key;
     QMap<Qt::Key, NES::Keys> &key_to_btn;
+    QKeySequenceEdit *m_sequence;
 };
 
 }  // namespace NES
--- src/environment/keymap_window.cpp
+++ src/environment/keymap_window.cpp
@@ -11,42 +11,30 @@ KeymapWindow::KeymapWindow(QMap<Qt::Key, NES::Keys> &key_to_btn_) : key_to_btn(k
     QUiLoader loader;
     loader.load(&file, this);
     QMetaObject::connectSlotsByName(this);
-
-    table = findChild<QTableWidget *>("table");
+    m_sequence = findChild<QKeySequenceEdit *>("sequence_edit");
+    m_table = findChild<QTableWidget *>("table_widget");
     for (auto &key : key_to_btn.keys()) {
         btn_to_key[key_to_btn[key]] = key;
         auto *cur = new QTableWidgetItem(QKeySequence(key).toString());
         cur->setTextAlignment(Qt::AlignCenter);
-        table->setItem(static_cast<int>(key_to_btn[key]), 1, cur);
-    }
-}
-
-void KeymapWindow::on_table_cellChanged(int row, int col) {
-    auto item = table->item(row, col);
-    auto seq = QKeySequence(item->text()).toString();
-    if (seq.isEmpty() || seq.count('+') != 0) {
-        QMessageBox::warning(this,
-                             "Bad key",
-                             "Only single character bindings are allowed!",
-                             (QMessageBox::Ok));
-        item->setText(QKeySequence(btn_to_key[NES::Keys(row)]).toString());
+        m_table->setItem(static_cast<int>(key_to_btn[key]), 1, cur);
     }
 }
 
 void KeymapWindow::on_restore_btn_clicked() {  // TODO
-    table->item(0, 1)->setText("A");
-    table->item(1, 1)->setText("S");
-    table->item(2, 1)->setText("Tab");
-    table->item(3, 1)->setText("Return");
-    table->item(4, 1)->setText("Up");
-    table->item(5, 1)->setText("Down");
-    table->item(6, 1)->setText("Left");
-    table->item(7, 1)->setText("Right");
+    m_table->item(0, 1)->setText("A");
+    m_table->item(1, 1)->setText("S");
+    m_table->item(2, 1)->setText("Tab");
+    m_table->item(3, 1)->setText("Return");
+    m_table->item(4, 1)->setText("Up");
+    m_table->item(5, 1)->setText("Down");
+    m_table->item(6, 1)->setText("Left");
+    m_table->item(7, 1)->setText("Right");
 }
 
 void KeymapWindow::on_cancel_btn_clicked() {
     for (auto &btn : btn_to_key.keys()) {
-        table->item(static_cast<int>(btn), 1)->setText(QKeySequence(btn_to_key[btn]).toString());
+        m_table->item(static_cast<int>(btn), 1)->setText(QKeySequence(btn_to_key[btn]).toString());
     }
     close();
 }
@@ -55,7 +43,7 @@ void KeymapWindow::on_apply_btn_clicked() {
     QSettings settings("NAD", "NADNES");
     settings.beginGroup("keymap");
     for (int i = 0; i < 7; i++) {
-        auto seq = QKeySequence(table->item(i, 1)->text());
+        auto seq = QKeySequence(m_table->item(i, 1)->text());
         if (seq[0].key() != btn_to_key[NES::Keys(i)]) {
             key_to_btn.remove(btn_to_key[NES::Keys(i)]);
             key_to_btn.insert(seq[0].key(), NES::Keys(i));
@@ -70,6 +58,28 @@ void KeymapWindow::on_apply_btn_clicked() {
 void KeymapWindow::on_save_btn_clicked() {
     on_apply_btn_clicked();
     close();
+}
+
+void KeymapWindow::on_sequence_edit_editingFinished() {
+    if (m_table->selectedItems().isEmpty()) {
+        return;
+    }
+    auto cur = m_table->selectedItems()[0];
+    auto seq = m_sequence->keySequence();
+    auto str = seq.toString();
+    if (seq.count() != 1 || str.count("+") != 0 || str.isEmpty()) {
+        QMessageBox::warning(this,
+                             "Bad key",
+                             "Only single character bindings are allowed!",
+                             (QMessageBox::Ok));
+        cur->setText(QKeySequence(btn_to_key[NES::Keys(cur->row())]).toString());
+    } else {
+        cur->setText(str);
+    }
+}
+
+void KeymapWindow::on_table_widget_itemDoubleClicked() {
+    m_sequence->setFocus();
 }
 
 }  // namespace NES
