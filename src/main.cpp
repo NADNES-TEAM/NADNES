ee49dde5474a49a4c26adc8ab8178111d73569e1
das747 <40293014+das747@users.noreply.github.com>
2022-06-04 19:20:06 +0300

    added icon and bundle on mac

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,15 @@
 #include <QApplication>
 #include "main_controller.h"
+#include <filesystem>
+#include <iostream>
 
 int main(int argc, char *argv[]) {
+#ifdef __APPLE__
+    std::filesystem::path cur(argv[0]);
+    std::filesystem::current_path(cur.parent_path());
+//    std::cout << "test"  << std::filesystem::current_path() << std::endl;
+#endif
+
     QApplication application(argc, argv);
     MainController controller;
     return QApplication::exec();
